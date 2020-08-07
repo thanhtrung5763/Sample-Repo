8eac8def46acb8da7f1fb65b56677d24d007c45d
Thanh Trung <thanhtrung5763@gmail.com>
2020-08-07 14:32:43 +0700

    Removed some code

--- first.py
+++ first.py
@@ -1,14 +1,6 @@
-import sys
+
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-print(greet('Candy'))
-print(greet('James'))
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
